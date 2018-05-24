6f4045eca2bc04615ef9892e0a9fbee4ab6a8d88
hello_chenchen <welcomechenchen@gmail.com>
2018-05-25 02:02:16 +0800

    添加sizeof计算

--- cc_test_code/sizeof/sizeof.cc
+++ cc_test_code/sizeof/sizeof.cc
@@ -1,20 +1,31 @@
-#include <iostream>  
-#include <string>  
+#include <iostream>
+#include <string>
+#include <malloc.h>
 using namespace std;  
 
-int _tmain(int argc, _TCHAR* argv[])  
-{  
-    string s = "hellohwc";  
-    char * s1 = "hellohwc";  
-    char s2[] = "hellohwc";  
-    char s3[100];  
-    char* s4=(char*)malloc(100);  
-    void *s5=(void*)malloc(100);  
-    cout<<sizeof(s)<<endl;  
-    cout<<sizeof(s1)<<endl;  
-    cout<<sizeof(s2)<<endl;  
-    cout<<sizeof(s3)<<endl;  
-    cout<<sizeof(s4)<<endl;  
-    cout<<sizeof(s5)<<endl;  
-    return 0;  
-}  
+int func() {
+    string s = "hellohw";
+    char * s1 = "hellohw";
+    char s2[] = "hellohw";
+    int s3[100];
+    char* s4=(char*)malloc(100);
+    void *s5=(void*)malloc(100);
+    cout<<sizeof(s)<<endl;
+    cout<<sizeof(s1)<<endl;
+    cout<<sizeof(s2)<<endl;
+    cout<<sizeof(s3)<<endl;
+    cout<<sizeof(s4)<<endl;
+    cout<<sizeof(s5)<<endl;
+    cout<<sizeof(1)<<endl;
+
+    free(s4);
+    free(s5);
+    return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    func();
+    return 0;
+}
