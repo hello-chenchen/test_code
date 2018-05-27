4158087d966ccf195ccf9070c35d6554a96d3f44
hello_chenchen <welcomechenchen@gmail.com>
2018-05-27 19:20:38 +0800

    mk 1.0.1

--- cc_test_code/construct-destruct/src/main.cc
+++ cc_test_code/construct-destruct/src/main.cc
@@ -1,4 +1,5 @@
 #include "class_define.h"
+#include "class_e.h"
 
 ClassC c;
 int main(int argc, char const *argv[])
@@ -6,6 +7,7 @@ int main(int argc, char const *argv[])
     /* code */
     ClassA* pa = new ClassA();
     ClassB b;
+    ClassE e;
     static ClassD d;
     delete pa;
     return 0;
