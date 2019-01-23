3fcedd51fc62746a0a124542b17ba1e78d8b1778
hello_chenchen <welcomechenchen@gmail.com>
2019-01-23 22:07:48 +0800

    test multiple namespace with the same method

---
+++ cc/multiple-namespace-same-method/multiple_namespace.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+namespace first
+{
+     void myCout(void)
+    {
+        cout<<"Hello World is great\n";
+    }
+}
+
+namespace second
+{
+     void myCout(void)
+    {
+        cout<<"Hello Sky is high\n";
+    }
+}
+
+
+int main(void)
+{
+    second::myCout();
+    first::myCout();
+    return(0);
+}
