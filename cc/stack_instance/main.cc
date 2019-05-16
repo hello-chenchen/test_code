776696955f1cc62d6517eeb33d40f0118e8dfb4d
hello_chenchen <welcomechenchen@gmail.com>
2019-05-16 20:57:37 +0800

    for test

---
+++ cc/stack_instance/main.cc
@@ -0,0 +1,10 @@
+#include "stack.h"
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    stack cc;
+    int aa;
+    cin>>aa;
+    return 0;
+}
---
+++ cc/stack_instance/stack.cc
@@ -0,0 +1,8 @@
+#include "stack.h"
+stack::stack() {
+    cout << "stack::new" << endl;
+}
+
+stack::~stack() {
+    cout << "stack::delete" << endl;
+}
